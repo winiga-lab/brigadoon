fcb65475b429449cf85d29adca0e8eed182351e1
mrzey <61865416+mrzey28@users.noreply.github.com>
2020-11-30 15:06:03 +0100

    Add files via upload
    Ajout api.php : Offre, Memberships, Noter

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\PersonneController;
+use App\Http\Controllers\OffreController;
+use App\Http\Controllers\MembershipsController;
+use App\Http\Controllers\NoterController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,4 +30,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::get('logout', [AuthController::class, 'logout']);
     Route::Resource('personnes', PersonneController::class);
+	Route::Resource('offre',OffreController::class);
+	Route::Resource('memberships',MembershipsController::class);
+	Route::Resource('memberships',NoterController::class);
 });
