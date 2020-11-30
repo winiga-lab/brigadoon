9f8df69f5ecf8b3943d7afc24caa0e6ddcc52243
user <user@tibo-PC.home>
2020-11-30 15:26:29 +0100

    Controller

---
+++ app/Http/Controllers/DiplomerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Diplomer;
+use Illuminate\Http\Request;
+
+class DiplomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Diplomer  $diplomer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Diplomer $diplomer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Diplomer  $diplomer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Diplomer $diplomer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Diplomer  $diplomer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Diplomer $diplomer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Diplomer  $diplomer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Diplomer $diplomer)
+    {
+        //
+    }
+}
